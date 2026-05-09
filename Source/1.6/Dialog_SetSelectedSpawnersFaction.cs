4c77df1ffa92f526fe819cf0c8262cd5d7ca8914
user <fldpi@tutanota.com>
2026-05-09 04:55:34 +0300

    Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.Added custom spawner multi-edit: when multiple custom spawners are selected, Set faction/Set pawnkind dialogs will correctly apply changes to all selected objects.

---
+++ Source/1.6/Dialog_SetSelectedSpawnersFaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+
+namespace SaveOurShip2
+{
+	public class Dialog_SetSelectedSpawnersFaction : Dialog_RenameShip
+	{
+		protected override void SetName(string name)
+		{
+			if (string.IsNullOrEmpty(name))
+				return;
+
+			// Will rename all selected spawners, just easy implementation for one gizmo working for 
+			// renaming multiple objects using dialog.
+			List<ThingWithComps> things = Find.Selector.SelectedObjects.OfType<ThingWithComps>().ToList();
+			foreach(ThingWithComps thing in things)
+			{
+				CompNameMe comp = thing.TryGetComp<CompNameMe>();
+				if (comp != null)
+				{
+					comp.factionDef = name;
+				}
+			}
+		}
+	}
+}
